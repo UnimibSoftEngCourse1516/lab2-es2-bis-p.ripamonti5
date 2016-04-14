b7cf34ccf2e61976587e016a240750001168f437
Paolo Ripamonti <p.ripamonti@lutech.it>
2016-04-14 17:36:48 +0200

    Fisrt commit by developer 1

---
+++ src/lab2_1/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package lab2_1;
+
+/**
+ * @author Paolo Ripamonti 
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		sayHelloByDeveloperOne();
+	}
+
+	private static void sayHelloByDeveloperOne() {
+		System.out.println("Hello from Developer #1");
+	}
+
+}
