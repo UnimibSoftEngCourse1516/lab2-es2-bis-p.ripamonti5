f26b7b5cefeb85c3a58b178aa2f3ccb9df52c1ef
Paolo Ripamonti <p.ripamonti@lutech.it>
2016-04-14 17:37:16 +0200

    First Commit by developer 2

---
+++ src/lab2_1/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package lab2_1;
+
+/**
+ * @author Paolo Ripamonti 
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		sayHelloByDeveloperTwo();
+	}
+
+	private static void sayHelloByDeveloperTwo() {
+		System.out.println("Hello from Developer #2");
+	}
+
+}
